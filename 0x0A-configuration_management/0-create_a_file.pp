fe783598c61e3e8d35b885de517ebb47f6e75954
Jimykiz <raphaelemerald2002@gmail.com>
2023-03-24 08:37:48 -0700

    all tasks

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-# Create a file in /tmp using Puppet
-
-file { 'holberton':
-  ensure  => present,
-  path    => '/tmp/holberton',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
+# This manifest creates a file at /tmp
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
